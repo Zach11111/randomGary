843aed6687c1856149a55ede6b846855a47880df
Gary <meow>
2024-10-17 14:19:19 -0500

    initial commit

---
+++ index.tsx
@@ -0,0 +1,9 @@
+import definePlugin from "@utils/types";
+import { addChatBarButton, removeChatBarButton } from "@api/ChatButtons";
+
+
+export default definePlugin({
+    name: "RandomGary",
+    description: "Adds a button to send a random gary picture!",
+    authors: [{ name: "Zach Orange", id: 683550738198036516n }],
+});
